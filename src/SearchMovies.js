2e3b8af1f77141a63c98d303b774d2ae3aca430c
Aya Elsayed <ayah.ehab11@gmail.com>
2021-05-01 09:30:54 +0100

    Add material UI filter button

--- src/SearchMovies.js
+++ src/SearchMovies.js
@@ -1,9 +1,35 @@
 import React, {useState} from "react"
+import {withStyles} from '@material-ui/core/styles';
+import IconButton from "@material-ui/core/IconButton";
+import FilterList from "@material-ui/icons/FilterList";
 import MovieCard from "./MovieCard"
 
 
-export default function SearchMovies() {
+const FilterButton = withStyles({
+    root: {
+      boxShadow: 'none',
+      color: "white",
+      backgroundColor: 'rgba(0,0,0,0.75)',
+      borderColor: 'rgba(0,0,0,0.75)',
+      '&:hover': {
+        backgroundColor: 'rgba(0,0,0,0.85)',
+        borderColor: 'rgba(0,0,0,0.85)',
+        boxShadow: 'none',
+      },
+      '&:active': {
+        boxShadow: 'none',
+        backgroundColor: 'rgba(0,0,0,0.65)',
+        borderColor: 'rgba(0,0,0,0.75)',
+      },
+      '&:focus': {
+          backgroundColor: 'rgba(0,0,0,0.85)',
+          borderColor: 'rgba(0,0,0,0.85)',
+          boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+      }
+    },
+  })(IconButton);
 
+export default function SearchMovies() {
     // states - query, movies
     const [query, setQuery] = useState('')
     const [movies, setMovies] = useState([])
@@ -32,7 +58,11 @@ export default function SearchMovies() {
                     placeholder="What do you want to watch?"
                     onChange={(e) => setQuery(e.target.value)}
                 />
-                <button className="button" type="submit">Search</button>
+                <button className="button" type="submit" disabled={!query}>Search</button>
+                {/* <button className="button" type="button"><FilterList /></button> */}
+                <FilterButton component="span">
+                    <FilterList />
+                </FilterButton>
             </form>
             <div className="card-list">
                 {movies.filter(movie => movie.poster_path).map(movie => <MovieCard key={movie.id} movie={movie}/>)}
